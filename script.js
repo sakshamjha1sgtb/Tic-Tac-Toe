0f49b66434e771532dd19d96f4ccb1047c0df58b
sakshamjha1sgtb <88288957+sakshamjha1sgtb@users.noreply.github.com>
2021-09-03 18:58:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById("result").style.display="none";
+document.getElementById("turn1").style.color="#00ff00";
 var id = 1;
 var p1 = document.getElementById('p1');
 var p2 = document.getElementById('p2');
@@ -9,14 +11,20 @@ var p7 = document.getElementById('p7');
 var p8 = document.getElementById('p8');
 var p9 = document.getElementById('p9');
 var b1=0,b2=1,b3=2,b4=3,b5=4,b6=5,b7=6,b8=7,b9=8,draw=0;
+
+
 function tic1(){
 	if(id==1 && b1==0){
 		id=2;
 		b1="O";
+		document.getElementById("turn2").style.color="#00ff00";
+		document.getElementById("turn1").style.color="#fff";
 		p1.style.color="white";
 		p1 =document.getElementById("none");
 	}
 	else if(b1==0){
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		id=1;
 		b1="X";
 		p1.innerHTML = "X";
@@ -28,12 +36,16 @@ function tic2(){
 	if(id==1 && b2==1){
 		id=2;
 		b2="O";
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		p2.style.color="white";
 		p2 = document.getElementById("none");
 	}
 	else if(b2==1){
 		id=1;
 		b2="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p2.innerHTML = "X";
 		p2.style.color="white";
 	}
@@ -44,11 +56,15 @@ function tic3(){
 		id=2;
 		b3='O';
 		p3.style.color="white";
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		p3 = document.getElementById("none");
 	}
 	else if(b3==2){
 		id=1;
 		b3="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p3.innerHTML = "X";
 		p3.style.color="white";
 	}
@@ -58,12 +74,16 @@ function tic4(){
 	if(id==1 && b4==3){
 		id=2;
 		b4="O";
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		p4.style.color="white";
 		p4 = document.getElementById("none");
 	}
 	else if(b4==3){
 		id=1;
 		b4="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p4.innerHTML = "X";
 		p4.style.color="white";
 	}
@@ -72,6 +92,8 @@ winner()
 function tic5(){
 	if(id==1 && b5==4){
 		id=2;
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		b5="O";
 		p5.style.color="white";
 		p5 = document.getElementById("none");
@@ -79,6 +101,8 @@ function tic5(){
 	else if(b5==4){
 		id=1;
 		b5="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p5.innerHTML = "X";
 		p5.style.color="white";
 	}
@@ -87,6 +111,8 @@ winner()
 function tic6(){
 	if(id==1 && b6==5){
 		id=2;
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		b6="O";
 		p6.style.color="white";
 		p6 = document.getElementById("none");
@@ -94,6 +120,8 @@ function tic6(){
 	else if(b6==5){
 		id=1;
 		b6="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p6.innerHTML = "X";
 		p6.style.color="white";
 	}
@@ -103,12 +131,16 @@ function tic7(){
 	if(id==1 && b7==6){
 		id=2;
 		b7="O";
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		p7.style.color="white";
 		p7 = document.getElementById("none");
 	}
 	else if(b7==6){
 		id=1;
 		b7="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p7.innerHTML = "X";
 		p7.style.color="white";
 	}
@@ -118,12 +150,16 @@ function tic8(){
 	if(id==1 && b8==7){
 		id=2;
 		b8="O";
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		p8.style.color="white";
 		p8 = document.getElementById("none");
 	}
 	else if(b8==7){
 		id=1;
 		b8="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p8.innerHTML = "X";
 		p8.style.color="white";
 	}
@@ -132,6 +168,8 @@ winner()
 function tic9(){
 	if(id==1 && b9==8){
 		id=2;
+		document.getElementById("turn1").style.color="#fff";
+		document.getElementById("turn2").style.color="#00ff00";
 		b9="O";
 		p9.style.color="white";
 		p9 = document.getElementById("none");
@@ -139,6 +177,8 @@ function tic9(){
 	else if(b9==8){
 		id=1;
 		b9="X";
+		document.getElementById("turn1").style.color="#00ff00";
+		document.getElementById("turn2").style.color="#fff";
 		p9.innerHTML = "X";
 		p9.style.color="white";
 	}
@@ -147,76 +187,120 @@ winner()
 
 function winner(){
 	if(b1==b2 && b2==b3 && b1=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p2').style.background="white";
+      document.getElementById('p3').style.background="white";
+		document.getElementById("result").style.display="contents";
+		
 	}
 	else if(b1==b4 && b4==b7 && b1=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p4').style.background="white";
+      document.getElementById('p7').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b1==b5 && b5==b9 && b1=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b2==b5 && b5==b8 && b2=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p2').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p8').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b3==b6 && b6==b9 && b3=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p3').style.background="white";
+      document.getElementById('p6').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b3==b5 && b5==b7 && b3=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p3').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p7').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b4==b5 && b5==b6 && b4=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p4').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p6').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b7==b8 && b8==b9 && b7=="O"){
-		alert("player 1  O won");
-		location.reload();
+		document.getElementById('p7').style.background="white";
+      document.getElementById('p8').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b1==b2 && b2==b3 && b1=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p2').style.background="white";
+      document.getElementById('p3').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b1==b4 && b4==b7 && b1=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p4').style.background="white";
+      document.getElementById('p7').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b1==b5 && b5==b9 && b1=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p1').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b2==b5 && b5==b8 && b2=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p2').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p8').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b3==b6 && b6==b9 && b3=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p3').style.background="white";
+      document.getElementById('p6').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b3==b5 && b5==b7 && b3=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p3').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p7').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b4==b5 && b5==b6 && b4=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p4').style.background="white";
+      document.getElementById('p5').style.background="white";
+      document.getElementById('p6').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else if(b7==b8 && b8==b9 && b7=="X"){
-		alert("player 2  X won");
-		location.reload();
+		document.getElementById('p7').style.background="white";
+      document.getElementById('p8').style.background="white";
+      document.getElementById('p9').style.background="white";
+		document.getElementById('result').innerHTML="Winner is player 2";
+		document.getElementById("result").style.display="contents";
 	}
 	else{
 		draw=draw+1;
 		if(draw==9){
-			alert("match draw");
-			location.reload;	
+		document.getElementById('result').innerHTML="Match Draw";
+		document.getElementById("result").style.display="contents";	
 		}
 	}
 	
 		
+}
+function reload(){
+	location.reload();
 }
